3a4e20fd7fc3de5c49169e1c9e63195681a2e9b1
Pavel Serebryakov <pavel.serebryakov@ilogos.biz>
2017-10-22 23:40:55 +0300

    main screen ui

---
+++ Assets/Scripts/Views/UI/MainMenuView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuView : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject PlaySubMenu;
+    public GameObject OptionsMenu;
+
+    public void OnPlayButtonClicked()
+    {
+        MainMenu.SetActive(false);
+        PlaySubMenu.SetActive(true);
+    }
+
+    public void OnPlayEasyButtonClicked()
+    {
+        
+    }
+
+    public void OnOptionsButtonClicked()
+    {
+        MainMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+
+    public void OnOptionsBackButtonClicked()
+    {
+        OptionsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void OnOptionsSaveButtonClicked()
+    {
+        OptionsMenu.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+
+}
